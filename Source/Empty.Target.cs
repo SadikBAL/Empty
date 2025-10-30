31c02e7044def4d162b4ce721a4564953170de32
SADIK\sadik <12609812+SadikBAL@users.noreply.github.com>
2025-10-30 17:39:28 +0300

    1

--- Source/Empty.Target.cs
+++ Source/Empty.Target.cs
@@ -9,7 +9,7 @@ public class EmptyTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("Empty");
 	}
 }
